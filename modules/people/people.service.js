77b873ecbc6847a87845a355ac231b12a5127cf2
M Jones <jonesman2800@gmail.com>
2020-08-27 11:55:06 -0400

    removed remaining  console logs

--- modules/people/people.service.js
+++ modules/people/people.service.js
@@ -13,13 +13,13 @@ store.people.forEach(person => people.enqueue(person));
 module.exports = {
   get() {
     // .log('all in queue',people.all());
-    console.log('people.get.all', people.all())
+    // console.log('people.get.all', people.all())
     return people.all();
   },
 
   enqueue(person) {
     people.enqueue(person);
-    console.log(`${person} joined adoption line`)
+    // console.log(`${person} joined adoption line`)
     return `${person} joined adoption line`;
   },
 
